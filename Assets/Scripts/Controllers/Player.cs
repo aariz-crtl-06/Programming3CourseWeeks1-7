5d73acb5deadd5887f6026ecbd5c992318f9fd92
aariz-crtl-06 <aarizza75@gmail.com>
2025-09-17 19:29:29 -0400

    Did task 1 of journal 2

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -6,13 +6,24 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public List<Transform> asteroidTransforms;
-    
+
+    public float numberOfTrailBombs = 3;
+    public int bombTrailSpacing;
+
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.B))
+        if (Input.GetKeyDown(KeyCode.B))
         {
-            SpawnBombAtOffset(new Vector3(0,1));
+            SpawnBombAtOffset(new Vector3(0, 1));
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            for (int i = 0; i < numberOfTrailBombs; i++)
+            {
+                SpawnBombTrail(bombTrailSpacing++);
+            }
         }
     }
 
@@ -21,5 +32,11 @@ public class Player : MonoBehaviour
 
         Vector3 spawnPosition = transform.position+inOffset;
         Instantiate(bombPrefab, spawnPosition, Quaternion.identity);
+    }
+
+    void SpawnBombTrail (float spacing)
+    {
+            Vector3 spaced = transform.position - new Vector3(0, spacing);
+            Instantiate(bombPrefab, spaced, Quaternion.identity);
     }
 }
